2ca9d19887d5db0a648e114266405e560cccbbe3
imi415 <imi415.public@gmail.com>
2021-08-10 19:08:14 +0800

    Removed docs, file permission changed to 0644.

--- cc13xxware_2_04_03_17272/doc/driverlib/search/groups_4.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['hs_5fcmd',['Hs_cmd',['../group__hs__cmd.html',1,'']]],
-  ['hsrxoutput',['HsRxOutput',['../group__hs_rx_output.html',1,'']]],
-  ['hsrxstatus',['HsRxStatus',['../group__hs_rx_status.html',1,'']]]
-];
